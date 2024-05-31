9fb87c5e0c093ab552683d5ff3d855f9d60ad1c9
Osama <osama.ayman1005@gmail.com>
2024-05-31 21:20:16 +0300

    Add cURL builder

---
+++ src/main/java/com/suitup/api/CurlBuilder.java
@@ -0,0 +1,38 @@
+package com.suitup.api;
+
+import java.util.Map;
+
+public class CurlBuilder {
+    static String generateQueryParameters(Map<String, Object> requestParameters) {
+        StringBuilder queryParamsBuilder = new StringBuilder();
+        requestParameters.forEach((key, value) ->
+                queryParamsBuilder.append(key).append("=").append(value).append("&"));
+        return queryParamsBuilder.toString();
+    }
+
+    static String createCurl(String requestMethod, String path,
+                             Map<String, Object> requestParameters,
+                             Map<String, Object> requestBody,
+                             Map<String, String> requestHeaders){
+        StringBuilder curlBuilder = new StringBuilder("curl ");
+        // Append base URL and path
+        curlBuilder.append("'").append(EnvironmentConfiguration.getBaseUrl())
+                .append("/").append(path).append("' \\ \n");
+        curlBuilder.append("-X '").append(requestMethod.toUpperCase()).append("' \\");
+        // Append headers
+        if (requestHeaders != null && !requestHeaders.isEmpty()) {
+            requestHeaders.forEach((key, value) ->
+                    curlBuilder.append("-H '").append(key).append(": ").append(value).append("' "));
+        }
+        // Append query parameters
+        if (requestParameters != null && !requestParameters.isEmpty()) {
+            curlBuilder.append("'").append(generateQueryParameters(requestParameters)).append("' ");
+        }
+        // Append request body
+        if (requestBody != null && !requestBody.isEmpty()) {
+            curlBuilder.append("-d '").append(requestBody.toString()).append("' ");
+        }
+
+        return curlBuilder.toString();
+    }
+}
