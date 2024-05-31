fd8f906187c6cd9526cf6fee11e9a093e0ce47ff
Osama <osama.ayman1005@gmail.com>
2024-05-31 21:21:08 +0300

    change authentication configuration to map and add curl to requests

--- src/main/java/com/suitup/api/ApiRequestExecutor.java
+++ src/main/java/com/suitup/api/ApiRequestExecutor.java
@@ -1,8 +1,6 @@
 package com.suitup.api;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,85 +9,89 @@ public abstract class ApiRequestExecutor {
     /*
      ################### POST ###################
      */
-    public static ApiResponse post(String path, Map<String, Object> requestBody){
+    public static ApiResult post(String path, Map<String, Object> requestBody){
         return post(path,requestBody, new HashMap<>());
     }
-    public static ApiResponse post(String path, Map<String, Object> requestBody,
-                                      Map<String, String> requestHeaders){
-        return new ApiResponse(RestAssured.given().when().spec(getSpecifications()).log().all()
+    public static ApiResult post(String path, Map<String, Object> requestBody,
+                                 Map<String, String> requestHeaders){
+        requestHeaders.putAll(AuthenticationConfiguration.getAuthenticationTokens());
+        String curl = CurlBuilder.createCurl("post",
+                path,null,requestBody,requestHeaders);
+        return new ApiResult(RestAssured.given().when()
                 .headers(requestHeaders)
                 .baseUri(EnvironmentConfiguration.getBaseUrl())
                 .body(requestBody)
-                .post(path));
+                .post(path), curl);
     }
     /*
     ################### PUT ###################
     */
-    public static ApiResponse put(String path, Map<String, Object> requestBody){
+    public static ApiResult put(String path, Map<String, Object> requestBody){
         return put(path, requestBody, new HashMap<>());
     }
-    public static ApiResponse put(String path, Map<String, Object> requestBody,
-                                     Map<String, String> requestHeaders){
-        return new ApiResponse(RestAssured.given().when().spec(getSpecifications())
+    public static ApiResult put(String path, Map<String, Object> requestBody,
+                                Map<String, String> requestHeaders){
+        requestHeaders.putAll(AuthenticationConfiguration.getAuthenticationTokens());
+        String curl = CurlBuilder.createCurl("put",
+                path,null,requestBody,requestHeaders);
+        return new ApiResult(RestAssured.given().when()
                 .headers(requestHeaders)
                 .baseUri(EnvironmentConfiguration.getBaseUrl())
                 .body(requestBody)
-                .put(path));
+                .put(path), curl);
     }
     /*
     ################### PATCH ###################
      */
-    public static ApiResponse patch(String path, Map<String, Object> requestBody){
+    public static ApiResult patch(String path, Map<String, Object> requestBody){
         return patch(path, requestBody, new HashMap<>());
     }
-    public static ApiResponse patch(String path, Map<String, Object> requestBody,
-                                       Map<String, String> requestHeaders){
-        return new ApiResponse(RestAssured.given().when().spec(getSpecifications())
+    public static ApiResult patch(String path, Map<String, Object> requestBody,
+                                  Map<String, String> requestHeaders){
+        requestHeaders.putAll(AuthenticationConfiguration.getAuthenticationTokens());
+        String curl = CurlBuilder.createCurl("patch",
+                path,null,requestBody,requestHeaders);
+        return new ApiResult(RestAssured.given().when()
                 .headers(requestHeaders)
                 .baseUri(EnvironmentConfiguration.getBaseUrl())
                 .body(requestBody)
-                .patch(path));
+                .patch(path), curl);
     }
     /*
     ################### GET ###################
      */
-    public static ApiResponse get(String path){
+    public static ApiResult get(String path){
         return get(path, new HashMap<>(), new HashMap<>());
     }
-    public static ApiResponse get(String path, Map<String, Object> requestParameters){
+    public static ApiResult get(String path, Map<String, Object> requestParameters){
         return get(path, requestParameters, new HashMap<>());
     }
-    public static ApiResponse get(String path, Map<String, Object> requestParameters,
-                                     Map<String, String> requestHeaders){
-        return new ApiResponse(RestAssured.given().when().spec(getSpecifications())
+    public static ApiResult get(String path, Map<String, Object> requestParameters,
+                                Map<String, String> requestHeaders){
+        requestHeaders.putAll(AuthenticationConfiguration.getAuthenticationTokens());
+        String curl = CurlBuilder.createCurl("get",
+                path,requestParameters,null,requestHeaders);
+        return new ApiResult(RestAssured.given().when()
                 .headers(requestHeaders)
                 .baseUri(EnvironmentConfiguration.getBaseUrl())
                 .params(requestParameters)
-                .get(path));
+                .get(path), curl);
     }
     /*
     ################### DELETE ###################
     */
-    public static ApiResponse delete(String path){
+    public static ApiResult delete(String path){
         return delete(path, new HashMap<>());
     }
-    public static ApiResponse delete(String path,  Map<String, String> requestHeaders){
-        return new ApiResponse(
-                RestAssured.given().when().spec(getSpecifications())
+    public static ApiResult delete(String path, Map<String, String> requestHeaders){
+        requestHeaders.putAll(AuthenticationConfiguration.getAuthenticationTokens());
+        String curl = CurlBuilder.createCurl("delete",
+                path,null,null,requestHeaders);
+        return new ApiResult(
+                RestAssured.given().when()
                         .headers(requestHeaders)
                         .baseUri(EnvironmentConfiguration.getBaseUrl())
-                        .delete(path));
-    }
-    private static RequestSpecification getSpecifications(){
-    RequestSpecification requestSpecification;
-    if (AuthenticationConfiguration.getAuthenticationSpecification()!=null){
-        requestSpecification = AuthenticationConfiguration.getAuthenticationSpecification();
-    }
-    else {
-        requestSpecification = new RequestSpecBuilder().build();
-    }
-    requestSpecification.header("Content-Type","application/json");
-    return requestSpecification;
+                        .delete(path), curl);
     }
 
 }
